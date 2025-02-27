d0540025a5715070e1f681a6bf797214b20e40f5
erik1502 <123226996+erik1502@users.noreply.github.com>
2025-02-27 11:03:46 +0800

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import PendingRequests from "./PendingRequests"; // Ensure this path is correct
+
+ReactDOM.render(<PendingRequests />, document.getElementById("pending-requests"));
