1da2fe34f293bfe3b6d20d42e10b4129410b5cb5
Lamatz <lamatond444@gmail.com>
2025-03-14 17:20:09 +0800

    fix: fixed the .ignorefile

--- Javascript/nav.js
+++ Javascript/nav.js
@@ -3,3 +3,19 @@ const hamBurger = document.querySelector(".toggle-btn");
 hamBurger.addEventListener("click", function () {
   document.querySelector(".sidebar").classList.toggle("expand");
 });
+
+
+
+function checkScreenSize() {
+  if (window.matchMedia("(max-width: 992px)").matches) {
+    document.querySelector(".sidebar").classList.remove("expand");
+  } else {
+    document.querySelector(".sidebar").classList.add("expand");
+  }
+}
+
+// Run on page load
+checkScreenSize();
+
+// Run on window resize
+window.addEventListener("resize", checkScreenSize);
